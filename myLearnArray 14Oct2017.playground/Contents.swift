71d044857a9e854c97a983587e74e9d002a104c5
polly <polleyrockysky@gmail.com>
2017-10-14 14:14:28 +0700

    Learning Array Success

--- myLearnArray 14Oct2017.playground/Contents.swift
+++ myLearnArray 14Oct2017.playground/Contents.swift
@@ -2,4 +2,38 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var nameArrayString = ["AAA", "BBB", "CCC"]
+
+print("nameArray index at 2 ==> \(nameArrayString[2])")
+
+
+//ให้แสดงผลของการบวกของ nameArrayString ตำแหน่งที่ 0 + "" + nameArrayString[3]
+
+let mySpace = "  "
+print("Answer ==> \(nameArrayString[0] + mySpace + nameArrayString[2])")
+
+//การหาขนาดของ Array
+let lengthArray = nameArrayString.count
+
+print("ขนาดของ Array มีค่า = \(lengthArray)")
+
+//การเพ่ิมสมาชิก ให้ Array
+
+nameArrayString.append("Doremon")
+
+nameArrayString.append("Doremy")
+
+nameArrayString.append("Jiant")
+
+//การลบสมาชิก Array
+nameArrayString.remove(at: 1)
+
+
+
+
+
+
+
+
+
+
