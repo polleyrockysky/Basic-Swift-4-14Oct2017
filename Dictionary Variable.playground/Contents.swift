8f653f37504df91e16bf6b15c856e516bc7ec1b5
polly <polleyrockysky@gmail.com>
2017-10-14 15:47:24 +0700

    Start Dictionary

---
+++ Dictionary Variable.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Dictionary Format เหมือน JSON เลยค่ะ
+
+var myDictionaryString = ["Key1":"Value1", "Key2":"Value2", "Key3":"Value3", "Key4":"Value4"]
+
+//การใช้งาน Dictionary
+
+print("myDictionaryString ที่มี Key มีค่าเท่ากับ Key2 คือ \(String(describing: myDictionaryString["Key2"]))")
+
+print("myDictionaryString ที่มี Key ที่มีค่าเท่ากับ Key3 คือ \(String(describing: myDictionaryString["Key3"]))")
+
+//การเพ่ิมสมาชิกให้ตัวแปร ประเภท dictionary
+myDictionaryString["Key4"] = "Doremon"
+
+
+print("Key ==> Key4 Value ==> \(String(describing: myDictionaryString["key4"]))")
+
+
+
+
+
